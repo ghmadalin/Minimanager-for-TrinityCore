26726eabb8a51987e23d99e427698e81a67026d3
Mythos <mythos94@hotmail.com>
2011-03-08 08:46:40 +0100

    [3] Added copyright headers
    Signed-off-by: Mythos <mythos94@hotmail.com>

--- header.php
+++ header.php
@@ -1,4 +1,22 @@
 <?php
+/*
+ *  Copyright (C) 2010-2011  TrinityScripts <http://www.trinityscripts.xe.cx/>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
 // Check if gzip is installed, if yes -> activate gzip compression
 //if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start("ob_gzhandler"); else ob_start();
 
--- lang/outdated/czech.php
+++ lang/outdated/czech.php
@@ -1,14 +1,20 @@
 <?php
 /*
- * Project Name: MiniManager for Trinity
- * Date: 20.5.2009 (0.13) 
- * Progress: 95% complete
- * From: http://www.heroesoffantasy.cz/trac/wow/browser/trunk/minimanager/lang/czech.php
- * Author: Chronos
- * Email: robie@centrum.cz
- * License: GNU General Public License v2(GPL)
+ *  Copyright (C) 2010-2011  TrinityScripts <http://www.trinityscripts.xe.cx/>
  *
- * To-do: Translate TALENT, PET_ABILITY, CHAR_AURA  section
+ *  This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
  
 
